223d2c2d11e5be0dcb87918ddbcfc9d109d24518
Flavio <alvaradotucto@gmail.com>
2023-06-11 21:58:01 -0500

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import createApp from './app';
 
 (async () => {
-  const port = 3000;
+  const port = process.env.PORT;
   const httpServer = await createApp();
   httpServer.listen(port, () => {
     console.log(`server running on port http://localhost:${port}`);
